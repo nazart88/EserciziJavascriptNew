77c3c6064ed19d5da0ef2bde926ddbbcfc19c685
nazart88 <parlato.developing@gmail.com>
2022-08-31 11:05:50 +0200

    svolgimento esercizio 38

--- esercizio 38/exercise-5.js
+++ esercizio 38/exercise-5.js
@@ -18,9 +18,23 @@ class BankAccount {
   }
 }
 
+class BankAccountVip extends BankAccount {
+  constructor(initialAmount) {
+    super(initialAmount)
+  }
+
+  deposit(amount) {
+    if (amount >= 1000) {
+      return super.deposit(amount + (amount *3)/100);
+    } else {
+      return super.deposit(amount)
+    }
+  }
+}
+
 const bankAccountVip = new BankAccountVip(1000);
-bankAccountVip.deposit(500);
+bankAccountVip.deposit(2200);
 bankAccountVip.deposit(1200);
-bankAccountVip.withdraw(800);
-bankAccountVip.deposit(3500);
+bankAccountVip.withdraw(2800);
+bankAccountVip.deposit(1100);
 bankAccountVip.view();
