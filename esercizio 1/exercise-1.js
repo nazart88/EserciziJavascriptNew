a0e88f0307fe0885d533ed882d85b0a8173630ad
nazart88 <parlato.developing@gmail.com>
2022-09-07 11:36:10 +0200

    svolgimento esercizio 1

---
+++ esercizio 1/exercise-1.js
@@ -0,0 +1,11 @@
+function canPlay() {
+  let personName = "Paul";
+  if (true) {
+  }
+
+  personName += " plays football"; // Argh! personName is not defined
+
+  console.log(personName);
+}
+
+canPlay();
